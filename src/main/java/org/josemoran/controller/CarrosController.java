09cc94b717bd711b89d987fce6d3b90b67721ef1
DevJosL <jmoran-2022388@kinal.edu.gt>
2025-07-11 18:43:55 -0600

    Se crearon las vistas de todas las tablas de la DataBase

--- src/main/java/org/josemoran/controller/CarrosController.java
+++ src/main/java/org/josemoran/controller/CarrosController.java
@@ -1,14 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package org.josemoran.controller;
+
+import org.josemoran.system.Main;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
-import org.josemoran.system.Main;
+import java.util.ArrayList;
 
+import java.sql.Date;
+import java.sql.Connection;
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 /**
  * FXML Controller class
  *
