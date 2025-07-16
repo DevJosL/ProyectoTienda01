3fb29464f3568daf1d82d3d9118bb326a7664278
DevJosL <jmoran-2022388@kinal.edu.gt>
2025-07-16 13:38:14 -0600

    Se realizo el inicio se sesión y el register de la aplicación

--- src/main/java/org/josemoran/controller/InicioSesionController.java
+++ src/main/java/org/josemoran/controller/InicioSesionController.java
@@ -5,8 +5,17 @@
 package org.josemoran.controller;
 
 import java.net.URL;
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import org.josemoran.database.Conexion;
 import org.josemoran.system.Main;
 
 /**
@@ -15,27 +24,56 @@ import org.josemoran.system.Main;
  * @author josel
  */
 public class InicioSesionController implements Initializable {
+
     private Main principal;
+
+    @FXML
+    private TextField txtCorreo;
+    
+    @FXML
+    private PasswordField pssContraseña;
+    
+    @FXML
+    private Text txtError;
 
     public void setPrincipal(Main principal) {
         this.principal = principal;
     }
-    
-    public void MenuView(){
-        principal.MenuScene();
-    }
-    
-    public void Volver(){
+
+    public void Volver() {
         principal.inicio();
     }
-    
-    public void RegistroView(){
+
+    public void RegistroView() {
         principal.RegistroScene();
     }
-    
+
+    public void ingresar() {
+
+        try {
+            CallableStatement enunciado = Conexion.getInstancia().getConexion()
+                    .prepareCall("call sp_loginUsuario(?,?);");
+            enunciado.setString(1, txtCorreo.getText());
+            enunciado.setString(2, pssContraseña.getText());
+            ResultSet resultado = enunciado.executeQuery();
+            if (resultado.next()) {
+                String mensaje = resultado.getString(1);
+
+                if ("Autorizado".equals(mensaje)) {
+                    principal.MenuScene();
+                    System.out.println("Autorizado");
+                } else {
+                    System.out.println("Acceso denegado");
+                    txtError.setVisible(true);
+                }
+            }
+        } catch (SQLException ex) {
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
-    }    
-    
+        Platform.runLater(() -> txtCorreo.requestFocus());
+    }
+
 }
--- src/main/java/org/josemoran/controller/RegistroController.java
+++ src/main/java/org/josemoran/controller/RegistroController.java
@@ -5,8 +5,17 @@
 package org.josemoran.controller;
 
 import java.net.URL;
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import org.josemoran.database.Conexion;
 import org.josemoran.system.Main;
 
 /**
@@ -16,14 +25,62 @@ import org.josemoran.system.Main;
  */
 public class RegistroController implements Initializable {
     private Main principal;
-
+    
+    @FXML
+    private TextField txtNombre, txtApellido,
+            txtCorreo, txtTelefono, txtDireccion;
+    
+    @FXML
+    private PasswordField pssContrasena;
+    
+    @FXML
+    private Text txtError;
+    
     public void setPrincipal(Main principal) {
         this.principal = principal;
     }
     
+    public void LoginScene(){
+        principal.InicioSesionScene();
+    }
+    
+    public void volver(){
+        principal.inicio();
+    }
+    
+    public void crearCuenta(){
+        if(txtNombre.getText().isBlank() ||
+                txtApellido.getText().isBlank() ||
+                txtCorreo.getText().isBlank() ||
+                pssContrasena.getText().isBlank() ||
+                txtTelefono.getText().isBlank() ||
+                txtDireccion.getText().isBlank()){
+                txtError.setVisible(true);
+    } else{
+            try {
+            CallableStatement enunciado = Conexion.getInstancia().getConexion()
+                    .prepareCall("call sp_AgregarUsuario(?,?,?,?,?,?);");
+            enunciado.setString(1, txtNombre.getText());
+            enunciado.setString(2, txtApellido.getText());
+            enunciado.setString(3, txtCorreo.getText());
+            enunciado.setString(4, pssContrasena.getText());
+            enunciado.setString(5, txtTelefono.getText());
+            enunciado.setString(6, txtDireccion.getText());
+            enunciado.executeQuery();
+            
+            System.out.println("Usuarios agregados correctamente");
+        } catch (SQLException ex) {
+            System.out.println("Error al agregar Usuarios " + ex.getMessage());
+            ex.printStackTrace();
+        }
+            principal.MenuScene();
+        }
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        Platform.runLater(() -> txtNombre.requestFocus());
+        
     }    
     
 }
